72ebebf4f8744263fd16ec14ccd9ac3296866724
AngelosGian <angelos.giannourakos@gmail.com>
2023-02-08 21:15:03 +0200

    adding dashboard and correcting partials

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,14 @@
 module.exports = {
     getIndex: (req, res) => {
         res.render('index.ejs')
+    },
+    getSignUp: (req, res) => {
+        res.render('signup.ejs')
+    },
+    getLogin: (req, res) => {
+        res.render('login.ejs')
+    },
+    getDashboard: (req, res) => {
+        res.render('dashboard.ejs')
     }
 }
--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,9 @@ const homeController = require('../controllers/home');
 
 
 router.get('/', homeController.getIndex)
+router.get('/signup', homeController.getSignUp);
+router.get('/login', homeController.getLogin);
+router.get('/dashboard', homeController.getDashboard);
 
 
 module.exports = router
